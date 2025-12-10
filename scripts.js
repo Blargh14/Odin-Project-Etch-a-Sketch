2da7b4c2cb399b4b3dcd07cc3f9569bddf900003
Blargh14 <59298248+Blargh14@users.noreply.github.com>
2025-12-10 03:28:07 -0500

    Add a trail to the grid with mousemove

--- scripts.js
+++ scripts.js
@@ -2,10 +2,19 @@ grid = document.querySelector("#container");
 
 for (let i = 0; i < 256; i++) {
     gridPiece = document.createElement("div");
-    gridPiece.style.backgroundColor = "white";
+    gridPiece.style.backgroundColor = "grey";
     gridPiece.style.aspectRatio = "1/1";
     gridPiece.style.height = "calc(6%)";
     gridPiece.style.width = "calc(6%)";
-    gridPiece.textContent = i;
     grid.appendChild(gridPiece);
+}
+
+grid.addEventListener("mousemove", changeGridPiece)
+
+function changeGridPiece(event) {
+    if (event.target === event.currentTarget) {
+        return;
+    }
+
+    event.target.style.backgroundColor = "black";
 }
